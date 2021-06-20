f466c885f4ebb44dd6e706f9f9cea881665a2c38
Skelebrine <70223021+Skelebrine@users.noreply.github.com>
2021-06-20 13:06:07 +0100

    extra tweaks

---
+++ matrixrunner.bat
@@ -0,0 +1,4 @@
+@echo off
+cd applications/other && start matrix.bat
+timeout /t 1 /nobreak
+exit /b 0
---
+++ noterunner.bat
@@ -0,0 +1,4 @@
+@echo off
+cd applications/main && start notecreator.bat
+timeout /t 1 /nobreak
+exit /b 0
